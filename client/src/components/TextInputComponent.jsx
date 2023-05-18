5a41a22069a4dbc453c3a8114837fbf7e3dc8ff0
DJ-IIITS <divyanshu2003dj@gmail.com>
2023-05-18 19:12:15 +0530

    TL-12: Add Text Input Component

--- client/src/components/TextInputComponent.jsx
+++ client/src/components/TextInputComponent.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { TextField } from "@mui/material";
+import '../App.css';
 
 const TextInput = (props) => {
-    const {value="Write your task", actionOnChange = () => console.log("Inside TextInput component")} = props;
+    const {value="", placeholder="", styling="", variant="outlined", actionOnChange = () => {}} = props;
     
     return (
       <div>
-          <TextField onChange={actionOnChange} value={value} placeholder="Enter your task" id="outlined-basic" variant="outlined" />
+          <TextField styling={styling} onChange={actionOnChange} value={value} placeholder={placeholder} id="text-input" variant={variant} />
       </div>
     );
 }
