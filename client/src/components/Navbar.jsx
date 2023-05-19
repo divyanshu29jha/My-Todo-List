4e484ea1b5a8e9f9ef53c18b91bd494e9acdcceb
kiran_ankita09 <ankita.kiran@trica.co>
2023-05-19 15:34:59 +0530

    TL-15 minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-function Navbar() {
+const useStyles = makeStyles((theme) => ({
+  appBar: (customStyle) => ({
+    marginBottom: theme.spacing(2),
+    ...customStyle,
+  }),
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Navbar(props) {
+  const { items, customStyle } = props;
+  const classes = useStyles(customStyle);
+
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.appBar}>
       <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Todo List
+        <Typography variant="h6" className={classes.title}>
+         
         </Typography>
+        <ul style={{ display: 'flex', listStyle: 'none' }}>
+          {items.map((item, index) => (
+            <li key={index} style={{ marginLeft: '10px' }}>
+              <a href={item.url}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
       </Toolbar>
     </AppBar>
   );
 }
+
+Navbar.defaultProps = {
+  items: [
+    { label: 'Home', url: '/' },
+    { label: 'About', url: '/about' },
+    { label: 'Services', url: '/services' },
+    { label: 'Contact', url: '/contact' },
+  ],
+  customStyle: {},
+};
 
 export default Navbar;
