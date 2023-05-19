0e38e07305107b37a815f9213165a11202a52bc8
kiran_ankita09 <ankita.kiran@trica.co>
2023-05-19 14:08:20 +0530

    TL-28: Added Default Values

--- client/src/components/CustomDialog.jsx
+++ client/src/components/CustomDialog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 
 function CustomDialog(props) {
-  const { open = false, onClose = () => {}, customStyle = {}, dialogContent = null } = props;
+  const { open = false, onClose = () => {}, customStyle = {}, dialogContent = "ADD TASK" } = props;
 
   return (
     <Dialog open={open} onClose={onClose} style={customStyle}>
