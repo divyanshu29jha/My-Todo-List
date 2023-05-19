1e0055ebef0249a3dd5dafbb0af851b62819451e
kiran_ankita09 <ankita.kiran@trica.co>
2023-05-19 11:31:06 +0530

    TL_28_Master_DialogueBox minor changes

--- client/src/components/CustomDialog.jsx
+++ client/src/components/CustomDialog.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 
 function CustomDialog(props) {
-  const { open, onClose, customStyle } = props;
+  const { open, onClose, customStyle, dialogContent } = props;
 
   return (
     <Dialog open={open} onClose={onClose} style={customStyle}>
-      {"add task"}
+      {dialogContent}
     </Dialog>
   );
 }
