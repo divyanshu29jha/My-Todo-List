6dfaf8fd60a01b7cdbfb3c4b0d6510c7915c28ce
kiran_ankita09 <ankita.kiran@trica.co>
2023-05-19 12:49:06 +0530

    TL-28: Incorporated Default values

--- client/src/components/CustomDialog.jsx
+++ client/src/components/CustomDialog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 
 function CustomDialog(props) {
-  const { open, onClose, customStyle, dialogContent } = props;
+  const { open = false, onClose = () => {}, customStyle = {}, dialogContent = null } = props;
 
   return (
     <Dialog open={open} onClose={onClose} style={customStyle}>
