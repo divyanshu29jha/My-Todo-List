d8043a9d2ed02445cce84f42027ed23ce0d9bf8a
saurabhbandkar28 <saurabhbandkar@gmail.com>
2023-05-22 13:29:56 +0530

    TL-17: add index

--- models/Todos.js
+++ models/Todos.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const TodosSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const TodosSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  dueDate: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+TodosSchema.index({ title: 1 }, { unique: true });
 
 module.exports = Todos = mongoose.model("todos", TodosSchema);
